bb8e2d6bb1edf0d5663a7d5659410e3b9c2db3df
flachyjoe <flachyjoe@gmail.com>
2021-09-30 11:55:43 +0200

    Update English translation

---
+++ Translations/FCPDWorkbench_en.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="en">
+<context>
+    <name>FCPDWorkbench</name>
+    <message>
+        <location filename="../InitGui.py" line="37"/>
+        <source>Pure-Data connection</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>FCPD_AddInclude</name>
+    <message>
+        <location filename="../fcpdwb_commands.py" line="140"/>
+        <source>Create a PDInclude object</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../fcpdwb_commands.py" line="141"/>
+        <source>Create a PDInclude object to store a PD patch in the FreeCAD document.</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>FCPD_Launch</name>
+    <message>
+        <location filename="../fcpdwb_commands.py" line="49"/>
+        <source>Launch Pure-Data</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../fcpdwb_commands.py" line="50"/>
+        <source>Launch Pure-Data and connect it to the internal server.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../fcpdwb_commands.py" line="81"/>
+        <source>Pure-Data is already running.
+</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>FCPD_Run</name>
+    <message>
+        <location filename="../fcpdwb_commands.py" line="96"/>
+        <source>Run Pure-Data server</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../fcpdwb_commands.py" line="97"/>
+        <source>Run the internal server and let Pure-Data to connect to.</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>FCPD_Stop</name>
+    <message>
+        <location filename="../fcpdwb_commands.py" line="120"/>
+        <source>Stop Pure-Data server</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../fcpdwb_commands.py" line="121"/>
+        <source>Stop the internal Pure-Data server.</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
